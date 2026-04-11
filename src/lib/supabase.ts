1f2ec1601629e6742ed71070e874e9f0bdfd77b7
joaosollatori-dotcom <joaosollatori@gmail.com>
2026-04-11 14:13:35 -0500

    correção de clip, criação de backend e transição pra supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL as string;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const url = 'https://iskocxmileyzbrjxwhbh.supabase.co';
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlza29jeG1pbGV5emJyanh3aGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MjUzMjcsImV4cCI6MjA5MTUwMTMyN30.VAvGg0s5nNBIIaKtEQGz_A776ubaERD7PC_swXesbd0';
 
 export const supabase = createClient(url, key);
 export type Database = typeof supabase;
