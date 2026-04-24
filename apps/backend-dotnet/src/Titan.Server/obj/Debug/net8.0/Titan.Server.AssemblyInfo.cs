38d9c8cde3fa6600ea7151fcfcddff28f0bff7a0
joaosollatori-dotcom <joaosollatori@gmail.com>
2026-04-24 07:53:27 -0500

    TITÃ | ISP - v 2.08.18

--- apps/backend-dotnet/src/Titan.Server/obj/Debug/net8.0/Titan.Server.AssemblyInfo.cs
+++ apps/backend-dotnet/src/Titan.Server/obj/Debug/net8.0/Titan.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Titan.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb6b057c59b7563e9596c5cb05491a7c5c2925e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8938fe8fba28fa08130c694c923a21b2f6815c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Titan.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Titan.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
